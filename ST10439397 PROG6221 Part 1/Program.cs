71a06b1480f0a29178a77850d9a68db8aa49fc4b
ST10439397-Jordan-Small <164010511+ST10439397-Jordan-Small@users.noreply.github.com>
2025-04-21 18:11:53 +0200

    Creating separate classes for more organisation of the code

--- ST10439397 PROG6221 Part 1/Program.cs
+++ ST10439397 PROG6221 Part 1/Program.cs
@@ -12,8 +12,8 @@ namespace ST10439397_PROG6221_Part_1
         static void Main(string[] args)
         {
 
-            Greeting();
-            AsciiArt();
+            VoiceGreeting.Greeting();
+            ASCII.AsciiArt();
             Console.ForegroundColor = ConsoleColor.Blue;
             TextDelay("“Hello! Welcome to the Cybersecurity Awareness Bot. I’m here to help you stay safe online.");
 
@@ -42,7 +42,7 @@ namespace ST10439397_PROG6221_Part_1
             }
 
             //Method of ways to reply to user inputs.
-            Replies();
+            UserQuestions.Replies();
         }
 //------------------------------------------------------------------------------------------------------------------
 //------------------------------------------------------------------------------------------------------------------
@@ -56,141 +56,6 @@ namespace ST10439397_PROG6221_Part_1
                 System.Threading.Thread.Sleep(50);
             }
             Console.WriteLine();
-        }
-//------------------------------------------------------------------------------------------------------------------
-//------------------------------------------------------------------------------------------------------------------
-//Method that has the voice greetinng.
-        public static void Greeting()
-        {
-
-            SoundPlayer player = new SoundPlayer("Greeting.wav");
-            player.Play();
-
-        }
-//------------------------------------------------------------------------------------------------------------------
-//------------------------------------------------------------------------------------------------------------------
-//Method that has the Art that'll be displayed.
-        static void AsciiArt()
-        {
-
-        }
-//------------------------------------------------------------------------------------------------------------------
-//------------------------------------------------------------------------------------------------------------------
-//Method that asks what the user would like to answered.
-
-        static void Replies()
-        {
-            try
-            {
-                Console.ForegroundColor = ConsoleColor.Cyan;
-                Console.WriteLine("Questions you may ask aligning with information that I contain: ");
-                Console.WriteLine("How are you?");
-                Console.WriteLine("What is your purpose?");
-                Console.WriteLine("What can I ask you about?");
-                Console.WriteLine("If you'd like to exit the application enter 'exit': ");
-                Console.WriteLine("---------------------------------------------------------");
-                Console.ResetColor();
-
-                Console.ForegroundColor = ConsoleColor.Blue;
-                string question = Console.ReadLine();
-
-
-                //Switch statement to check the user's input and provide a response.
-                switch (question)
-                {
-                    case "How are you?":
-
-                        Console.ForegroundColor = ConsoleColor.Yellow;
-                        Console.WriteLine("Sorry I'm not programmed to have emotions or opinions,\n" +
-                            " but i do know how speak like Wally from WALL-E, if you'd like.");
-                        Console.WriteLine("---------------------------------------------------------");
-                        Replies();
-                        break;
-
-                    case "What is your purpose?":
-
-                        Console.ForegroundColor = ConsoleColor.Yellow;
-                        Console.WriteLine("My purpose is to help you stay safe online by providing information and answering your questions about cybersecurity.");
-                        Console.WriteLine("---------------------------------------------------------");
-                        Replies();
-                        break;
-
-                    case "What can I ask you about?":
-
-                        Info();
-                        break;
-
-                    case "exit":
-
-                        Environment.Exit(0);
-                        break;
-
-                    default:
-
-                        Console.ForegroundColor = ConsoleColor.Green;
-                        Console.WriteLine("I didn’t quite understand that. Could you rephrase?");
-                        Console.WriteLine("---------------------------------------------------------");
-                        Replies();
-                        break;
-                }
-
-            }
-            catch (Exception ex)
-            {
-                Console.WriteLine($"An error occurred: {ex.Message}");
-                return;
-            }
-        }
-//------------------------------------------------------------------------------------------------------------------
-//------------------------------------------------------------------------------------------------------------------
-//Method that contains the information to keep  users safe on the internet.
-        public static void Info()
-        {
-
-            Console.ForegroundColor = ConsoleColor.Red;
-            Console.WriteLine("You may ask me about information related to password safety, phishing, and safe browsing");
-            Console.WriteLine("Please enter your question: ");
-            Console.ResetColor();
-
-            Console.ForegroundColor = ConsoleColor.Blue;
-            string input = Console.ReadLine();
-
-            switch (input)
-            {
-                case "password safety":
-
-                    Console.ForegroundColor = ConsoleColor.Yellow;
-                    Console.WriteLine("Password safety is important. Use strong, unique passwords for each account and consider using a password manager.");
-                    Console.WriteLine("---------------------------------------------------------");
-                    Replies();
-                    break;
-
-                case "phishing":
-
-                    Console.ForegroundColor = ConsoleColor.Yellow;
-                    Console.WriteLine("Phishing is a method used by cybercriminals to trick you into giving them your personal information.\n" +
-                        "Be cautious of suspicious emails or messages.");
-                    Console.WriteLine("---------------------------------------------------------");
-                    Replies();
-                    break;
-
-                case "safe browsing":
-
-                    Console.ForegroundColor = ConsoleColor.Yellow;
-                    Console.WriteLine("Safe browsing involves being cautious about the websites you visit and the information you share online.\n" +
-                        "Always check for HTTPS in the URL.");
-                    Console.WriteLine("---------------------------------------------------------");
-                    Replies();
-                    break;
-
-                default:
-
-                    Console.ForegroundColor = ConsoleColor.Green;
-                    Console.WriteLine("I’m not sure about that. Please ask me about password safety, phishing, or safe browsing.");
-                    Console.WriteLine("---------------------------------------------------------");
-                    Info();
-                    break;
-            }
         }
     }
 }
---
+++ ST10439397 PROG6221 Part 1/UserQuestions.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ST10439397_PROG6221_Part_1
+{
+    internal class UserQuestions
+    {
+        public static void Replies()
+        {
+            try
+            {
+                Console.ForegroundColor = ConsoleColor.Cyan;
+                Console.WriteLine("Questions you may ask aligning with information that I contain: ");
+                Console.WriteLine("How are you?");
+                Console.WriteLine("What is your purpose?");
+                Console.WriteLine("What can I ask you about?");
+                Console.WriteLine("If you'd like to exit the application enter 'exit': ");
+                Console.WriteLine("---------------------------------------------------------");
+                Console.ResetColor();
+
+                Console.ForegroundColor = ConsoleColor.Blue;
+                string question = Console.ReadLine();
+
+
+                //Switch statement to check the user's input and provide a response.
+                switch (question)
+                {
+                    case "How are you?":
+
+                        Console.ForegroundColor = ConsoleColor.Yellow;
+                        Console.WriteLine("Sorry I'm not programmed with emotions or opinions.");
+                        Console.WriteLine("---------------------------------------------------------");
+                        Replies();
+                        break;
+
+                    case "What is your purpose?":
+
+                        Console.ForegroundColor = ConsoleColor.Yellow;
+                        Console.WriteLine("My purpose is to help you stay safe online by providing information and answering your questions about cybersecurity.");
+                        Console.WriteLine("---------------------------------------------------------");
+                        Replies();
+                        break;
+
+                    case "What can I ask you about?":
+
+                        Info();
+                        break;
+
+                    case "exit":
+
+                        Environment.Exit(0);
+                        break;
+
+                    default:
+
+                        Console.ForegroundColor = ConsoleColor.Green;
+                        Console.WriteLine("I didn’t quite understand that. Could you rephrase?");
+                        Console.WriteLine("---------------------------------------------------------");
+                        Replies();
+                        break;
+                }
+
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"An error occurred: {ex.Message}");
+                return;
+            }
+        }
+//------------------------------------------------------------------------------------------------------------------
+//------------------------------------------------------------------------------------------------------------------
+//Method that contains the information to keep  users safe on the internet.
+        public static void Info()
+        {
+
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine("You may ask me about information related to password safety, phishing, and safe browsing");
+            Console.WriteLine("Please enter your question: ");
+            Console.ResetColor();
+
+            Console.ForegroundColor = ConsoleColor.Blue;
+            string input = Console.ReadLine();
+
+            switch (input)
+            {
+                case "password safety":
+
+                    Console.ForegroundColor = ConsoleColor.Yellow;
+                    Console.WriteLine("Password safety is important. Use strong, unique passwords for each account and consider using a password manager.");
+                    Console.WriteLine("---------------------------------------------------------");
+                    Replies();
+                    break;
+
+                case "phishing":
+
+                    Console.ForegroundColor = ConsoleColor.Yellow;
+                    Console.WriteLine("Phishing is a method of obtaining personal information by impersonating a business or people of importance.");
+                    Console.WriteLine("---------------------------------------------------------");
+                    Replies();
+                    break;
+
+                case "safe browsing":
+
+                    Console.ForegroundColor = ConsoleColor.Yellow;
+                    Console.WriteLine("Safe browsing involves being cautious about the websites you visit and the information you share online.\n" +
+                        "Always check for HTTPS in the URL.");
+                    Console.WriteLine("---------------------------------------------------------");
+                    Replies();
+                    break;
+
+                default:
+
+                    Console.ForegroundColor = ConsoleColor.Green;
+                    Console.WriteLine("I’m not sure about that. Please ask me about password safety, phishing, or safe browsing.");
+                    Console.WriteLine("---------------------------------------------------------");
+                    Info();
+                    break;
+            }
+        }
+    }
+}
+//-----------------------------------------0000EndOfFile0000------------------------------------------
